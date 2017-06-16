539d3d96e6d1790996c41ee09d20cbfb617a9f75
shahar3 <shahar061@gmail.com>
2017-06-16 10:40:28 +0300

    dfd

---
+++ MyForum/MyForum/Model/MyModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyForum.Model
+{
+    class MyModel:INotifyPropertyChanged
+    {
+        public MyModel()
+        {
+
+        }
+
+#region event handler
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void notifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+#endregion
+    }
+}
---
+++ MyForum/MyForum/ViewModel/MyViewModel.cs
@@ -0,0 +1,31 @@
+﻿using MyForum.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyForum.ViewModel
+{
+    class MyViewModel:INotifyPropertyChanged
+    {
+        MyModel model;
+
+        public MyViewModel(MyModel model)
+        {
+            this.model = model;
+        }
+
+        #region event handler
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void notifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+        #endregion
+    }
+}
