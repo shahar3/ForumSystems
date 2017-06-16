83044cb54a6801e7d11d0e426a2f6fc2ffc10f1e
shahar3 <shahar061@gmail.com>
2017-06-16 10:54:56 +0300

    lkl;

---
+++ MyForum/MyForum/View/Controls/LogInC.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyForum.View.Controls
+{
+    /// <summary>
+    /// Interaction logic for LogInC.xaml
+    /// </summary>
+    public partial class LogInC : UserControl
+    {
+        public LogInC()
+        {
+            InitializeComponent();
+        }
+
+        //This event triggered when the user press on log in button
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
